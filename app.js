fa8c567955b3bb4ed09bd117ac2887a929491f18
Kirra Hyde <kirra.hyde@gmail.com>
2023-07-11 19:17:47 -0400

    made form and WIP addGiphy()

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log("Let's get this party started!");
+
+$("submit").on("click", addGiphy)
+
+async function addGiphy() {
+  let searchTerm = $("submit").val();
+  let giphy = await axios(`http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
+  $()
+}
