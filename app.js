9d4e2e5e140e50036f6b5165faa2bf3d851c5b90
Karly Stark <karlystark@gmail.com>
2023-07-11 16:44:57 -0700

    postGiphy function and addGiphy bugs

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 console.log("Let's get this party started!");
 
-$("submit").on("click", addGiphy)
+async function postGiphy(giphy) {
+  $("#giphy-canvas").append(`<img src="${giphy.url}">`);
+}
+
 
 async function addGiphy() {
-  let searchTerm = $("submit").val();
-  let giphy = await axios(`http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
-  $()
+  let searchTerm = $("#search-term").val();
+  console.log(searchTerm);
+  let giphy = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym&limit=1`);
+  console.log(giphy.data);
+  postGiphy(giphy);
 }
+
+$("#submit").on("click", function(e){
+  e.preventDefault();
+  addGiphy();});
+
